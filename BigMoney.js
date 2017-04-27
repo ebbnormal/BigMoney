dbc1f087fbb746dbef43491ff8203494a44d7c87
ebbnormal <Ryan.Wieghard@gmail.com>
2017-04-26 17:20:30 -0700

    Create BigMoney.js

---
+++ BigMoney.js
@@ -0,0 +1,17 @@
+var BigMoney = class BigMoney {
+    
+   constructor(number){
+      this.numberArray = readNumberIntoArray(number);
+   }
+    
+   static multiply(a, b) {
+     if a isinstanceof BigMoney && b isinstanceof BigMoney {
+   
+   
+     } else {
+       return;
+     }
+   }
+
+
+}

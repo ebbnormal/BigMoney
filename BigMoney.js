b65b8751ab044643f6e9a1573697b937fd05ef83
ebbnormal <Ryan.Wieghard@gmail.com>
2017-04-26 17:50:34 -0700

    Update BigMoney.js

--- BigMoney.js
+++ BigMoney.js
@@ -1,8 +1,29 @@
 var BigMoney = class BigMoney {
-    
+   const MAX_POSITIVE_FLOAT_BITS = 64;
    constructor(number){
       this.numberArray = readNumberIntoArray(number);
    }
+    
+   readNumberIntoArray(number) {
+      if numberCannotBeRepresentedPerfectlyAsFloat64(number) {
+
+      } else {
+          var buffer = new ArrayBuffer(8); 
+      }
+   }
+    
+    
+  numberCannotBeRepresentedPerfectlyAsFloat64(number) {
+    return numberIsDivisible64TimesBy2(number, 1);
+  }
+    
+  numberIsDivisible63TimesBy2(number, count) {
+      if (count == (MAX_POSITIVE_FLOAT_BITS + 1)) {
+          return number > 0;
+      }
+      return numberIsDivisible63TimesBy2(number / 2, count +1 );
+  }
+   
     
    static multiply(a, b) {
      if a isinstanceof BigMoney && b isinstanceof BigMoney {
@@ -12,6 +33,4 @@ var BigMoney = class BigMoney {
        return;
      }
    }
-
-
 }
